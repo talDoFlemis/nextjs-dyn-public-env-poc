171009988b39cdb4dc662897cb585fe986a7e792
Said Rodrigues <coderflemis@gmail.com>
2025-01-26 14:17:13 -0300

    feat: add jiti to env variables runtime

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from "next";
 
+import { fileURLToPath } from "url";
+
+import createJiti from "jiti";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+jiti("./src/env.ts");
+
 const nextConfig: NextConfig = {
   output: "standalone",
   /* config options here */
